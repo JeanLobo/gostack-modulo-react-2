76db6d677eaa3b72d32f1e2d8e191d95674fd4ef
Jean Lobo <jeanlobocd@gmail.com>
2019-09-01 20:31:34 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Main from './pages/Main';
+import Repository from './pages/Repository';
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Main} />
+        <Route path="/repository" component={Repository} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+// <BrowserRouter> => Permite a criação de rotas de acesso as paginas
+// <Switch> => Permite restringir o acesso a uma rota por vez

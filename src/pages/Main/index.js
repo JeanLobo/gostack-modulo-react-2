cfecf3c7201623821c4e4578937d1ad18fba99f0
Jean Lobo <jeanlobocd@gmail.com>
2019-09-01 21:27:02 -0300

    chore: Add services with axios

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,23 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+
+import api from '../../services/api';
 
 import { Container, Form, SubmitButton } from './styles';
 
-export default function Main() {
-  return (
-    <Container>
-      <h1>
-        <FaGithubAlt />
-        Repositorios
-      </h1>
+export default class Main extends Component {
+  state = {
+    newRepo: '',
+    repositories: [],
+    loading: false,
+  };
 
-      <Form onSubmit={() => {}}>
-        <input type="text" placeholder="Adicionar repositório" />
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  };
 
-        <SubmitButton disabled>
-          <FaPlus color="#FFF" size={14} />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+  handleSubmit = async e => {
+    e.preventDefault();
+    //console.log(this.state.newRepo);
+    this.setState({ loading: true });
+    const { newRepo, repositories } = this.state;
+    const response = await api.get(`/repos/${newRepo}`);
+    //console.log(response.data);
+    const data = {
+      name: response.data.full_name,
+    };
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: '',
+      loading: false,
+    });
+  };
+
+  render() {
+    const { newRepo, loading } = this.state;
+
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositorios
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adicionar repositório"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+
+          <SubmitButton loading={loading}>
+            <FaPlus color="#FFF" size={14} />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
